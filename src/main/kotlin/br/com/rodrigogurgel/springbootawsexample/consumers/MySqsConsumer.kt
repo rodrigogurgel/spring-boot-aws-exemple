99eeb6f1e972ee0ff579475cd263ac911f3a08e7
Rodrigo Gurgel <rodrigohenrique.gurgel@outlook.com>
2023-10-12 16:32:14 -0300

    add how much time was spend to send email

--- src/main/kotlin/br/com/rodrigogurgel/springbootawsexample/consumers/MySqsConsumer.kt
+++ src/main/kotlin/br/com/rodrigogurgel/springbootawsexample/consumers/MySqsConsumer.kt
@@ -1,6 +1,8 @@
 package br.com.rodrigogurgel.springbootawsexample.consumers
 
 import io.awspring.cloud.sqs.annotation.SqsListener
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
@@ -16,6 +18,7 @@ class MySqsConsumer(
         private val logger: Logger = LoggerFactory.getLogger(MySqsConsumer::class.java)
     }
 
+    @OptIn(ExperimentalTime::class)
     @SqsListener("my-queue")
     fun listen(@RequestBody email: String) {
         val randomNum = Math.random() * 10
@@ -26,7 +29,8 @@ class MySqsConsumer(
         message.subject = "Sent from SQS my-topic"
         message.text = "This message was send from SQS listener"
 
-        mailSender.send(message)
+        val duration = measureTime { mailSender.send(message) }
+        logger.info("Sent email to {} in {} ms", message.to, duration.inWholeMilliseconds)
     }
 
     @SqsListener("my-queue-dlq", factory = "dlq")

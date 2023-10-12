dbe06cf04db198945e1029be9c87f3b90bd402da
Rodrigo Gurgel <rodrigohenrique.gurgel@outlook.com>
2023-10-12 01:27:37 -0300

    add aws dependecies

---
+++ src/main/kotlin/br/com/rodrigogurgel/springbootawsexample/SpringBootAwsExampleApplication.kt
@@ -0,0 +1,11 @@
+package br.com.rodrigogurgel.springbootawsexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootAwsExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootAwsExampleApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/rodrigogurgel/springbootawsexample/SpringBootAwsExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.rodrigogurgel.springbootawsexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootAwsExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
